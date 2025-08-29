991b97f18cf375066dd1109ff0897094eafd65c8
ErikEikeland <erik.eikeland80@gmail.com>
2025-08-29 10:01:10 +0200

    Delete index.tsx

--- index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-// "Legacy" ReactDOM-rendering, støttes bedre i CodeSandbox
-ReactDOM.render(<App />, document.getElementById("root"));
