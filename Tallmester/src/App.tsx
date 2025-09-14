5bb0bff79d297154845d1e1235d9f3e8b13f32a0
ErikEikeland <erik.eikeland80@gmail.com>
2025-09-14 18:23:41 +0200

    Update App.tsx

--- Tallmester/src/App.tsx
+++ Tallmester/src/App.tsx
@@ -5,20 +5,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TallmesterApp from "./components/TallmesterApp"; // Lokal enkelversjon av spillet
 import GameMaster from "./components/GameMaster"; // Lærervisning, styrer spillet og viser QR-kode
 import PlayerClient from "./components/PlayerClient"; // Spillerklient for mobil, der elever leverer svar
+import WelcomeMenu from "./components/WelcomeMenu";
 
 export default function App() {
   return (
     // Oppretter en Router for navigasjon mellom ulike ruter/visninger
     <Router>
       <Routes>
-        {/* Rute for lokal spilling uten flerspiller. */}
-        <Route path="/" element={<TallmesterApp />} />
-
-        {/* Rute for lærerens skjerm der spillet styres. Viser spill-ID og QR-kode */}
-        <Route path="/game" element={<GameMaster />} />
-
-        {/* Rute for spillerens mobilvisning. gameId må være med i URL-en som query-param */}
-        <Route path="/join" element={<PlayerClient />} />
+         <Route path="/" element={<WelcomeMenu />} />
+        <Route path="/local" element={<TallmesterApp />} />
       </Routes>
     </Router>
   );
